65c763d6d9a556e030d49f93a6d1786737d05e3a
RTX121 <160412000+RTX121@users.noreply.github.com>
2025-06-28 11:50:40 +0500

    Create Month.cs

---
+++ Month.cs
@@ -0,0 +1,54 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int month = 4;
+    
+    switch(month){
+        case 1:
+        Console.WriteLine("It is Jan");
+        break;
+        case 2:
+        Console.WriteLine("It is Feb");
+        break;
+        case 3:
+        Console.WriteLine("It is March");
+        break;
+         case 4:
+        Console.WriteLine("It is April");
+        break;
+         case 5:
+        Console.WriteLine("It is May");
+        break;
+         case 6:
+        Console.WriteLine("It is June");
+        break;
+         case 7:
+        Console.WriteLine("It is July");
+        break;
+         case 8:
+        Console.WriteLine("It is Aug");
+        break;
+         case 9:
+        Console.WriteLine("It is Sep");
+        break;
+         case 10:
+        Console.WriteLine("It is Oct");
+        break;
+         case 11:
+        Console.WriteLine("It is Nov");
+        break;
+         case 12:
+        Console.WriteLine("It is Dec");
+        break;
+        default:
+        Console.WriteLine("None");
+        break;
+    }
+    }
+}
